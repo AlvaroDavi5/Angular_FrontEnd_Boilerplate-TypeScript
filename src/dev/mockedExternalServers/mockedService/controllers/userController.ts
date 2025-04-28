f59c560644ecd933a352b39ecd62151bb410267c
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2025-04-28 19:21:04 -0300

    chore: added scripts

---
+++ src/dev/mockedExternalServers/mockedService/controllers/userController.ts
@@ -0,0 +1,33 @@
+import { Router, Request, Response } from 'express';
+
+
+export default {
+	login: async (req: Request, res: Response) => {
+		const { authorization } = req.headers;
+		console.log(`#Path: ${req.path}`);
+		console.log('#Has Token: ', !!authorization);
+
+		const user = {
+			userId: 'b562b8b5-65cd-41c6-be15-087014f326a4',
+		};
+
+		try {
+			const data = {
+				token: 'XXX',
+				...user,
+			};
+
+			return res.status(200).send(data);
+		} catch (error) {
+			return res.status(5000).send(error);
+		}
+	},
+
+	router() {
+		const router = Router();
+
+		router.put('/users/', this.login);
+
+		return router;
+	},
+};
